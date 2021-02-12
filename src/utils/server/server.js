d0c31722f82a9460b46c212dc0648b5fcbe01b37
AW5048816 <71649397+AW5048816@users.noreply.github.com>
2021-02-12 12:02:39 +0530

    try heroku

--- src/utils/server/server.js
+++ src/utils/server/server.js
@@ -1,54 +1,13 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
-
-
+const path = require('path');
+const express = require('express');
 const app = express();
-app.use(cors({ origin: "*" }));
-app.use(bodyParser.json());
+const publicPath = path.join(__dirname, '../../', 'public');
 
-app.listen(3000, () => {
-  console.log("The server started on port 3000 !!!!!!");
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
-
-app.get("/", (req, res) => {
-  res.send(
-    "<h1 style='text-align: center'>Wellcome to FunOfHeuristic <br><br>😃👻😃👻😃👻😃👻😃</h1>"
-  );
+app.listen(port, () => {
+  console.log('Server is up!');
 });
-
-app.post("/sendmail", (req, res) => {
-  console.log("request came");
-  let user = req.body;
-  sendMail(user, info => {
-    console.log(`The mail has beed send 😃 and the id is ${info.messageId}`);
-    res.send(info);
-  });
-});
-
-async function sendMail(user, callback) {
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: "walunja1@gmail.com",
-      pass: "narayangaon"
-    }
-  });
-
-  let mailOptions = {
-    from: '"Fun Of Heuristic"<walunja1@gmail.com>', // sender address
-    to: "walunja11@gmail.com", // list of receivers
-    subject: "Wellcome to Fun Of Heuristic 👻", // Subject line
-    html: `<h1>Hi "Ameya"</h1><br>
-    <h4>Thanks for joining us</h4>`
-  };
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail(mailOptions);
-
-  callback(info);
-}
